85c733cec56afa65d142d1a1564db87a177bfaf5
Vasilis <38428015+vmyrgiotis@users.noreply.github.com>
2020-07-08 12:56:49 +0100

    Update calculator_soil_texture.py

--- calculator_soil_texture.py
+++ calculator_soil_texture.py
@@ -1,36 +1,40 @@
+def soiltext(cl,si,sa):
+
     if (0 < cl < 10) and (0 < si < 15) and ( 85 < sa < 100):
-        texture = 'SAND'
+        texture = 'sand'
         WP2FC = 0.50
     if (0 < cl < 15) and (0 < si < 30) and ( 70 < sa < 90):
-        texture = 'LOSA'
+        texture = 'loamy_sand'
         WP2FC = 0.41
     if (0 < cl < 20) and (0 < si < 50) and ( 43 < sa < 85):
-        texture = 'SALO'
+        texture = 'sandy_loam'
         WP2FC = 0.44
     if (35 < cl < 55) and (0 < si < 20) and ( 45 < sa < 65):
-        texture = 'SNCL'
+        texture = 'sandy_clay'
         WP2FC = 0.63
-    if (27 < cl < 40) and (40 < si < 73) and ( 0 < sa < 20):
-        texture = 'SCLO'
+    if (25 < cl < 40) and (40 < si < 75) and ( 0 < sa < 20):
+        texture = 'silty_clay_loam'
         WP2FC = 0.70
-    if (6 < cl < 28) and (26 < si < 50) and ( 23 < sa < 53):
-        texture = 'LOAM'
+    if (5 < cl < 39) and (25 < si < 50) and ( 25 < sa < 55):
+        texture = 'loam'
         WP2FC = 0.50
-    if (0 < cl < 27) and (50 < si < 88) and ( 0 < sa < 50):
-        texture = 'SILO'
+    if (0 < cl < 27) and (50 < si < 100) and ( 0 < sa < 50):
+        texture = 'silt_loam'
         WP2FC = 0.35
-    if (27 < cl < 40) and (15 < si < 53) and ( 20 < sa < 45):
-        texture = 'CLLO'
+    if (27 < cl < 40) and (15 < si < 55) and ( 20 < sa < 45):
+        texture = 'clay_loam'
         WP2FC = 0.61
     if (0 < cl < 12) and (80 < si < 100) and ( 0 < sa < 20):
-        texture = 'SILT'
+        texture = 'silt'
         WP2FC = 0.20
-    if (20 < cl < 35) and ( 0 < si < 20) and ( 45 < sa < 80 ):
-        texture = 'SCLO'
+    if (20 < cl < 35) and ( 0 < si < 30) and ( 45 < sa < 80 ):
+        texture = 'sandy_clay_loam'
         WP2FC = 0.58
     if (40 < cl < 60) and (40 < si < 60) and ( 0 < sa < 20):
-        texture = 'SICL'
+        texture = 'silty_clay'
         WP2FC = 0.66
     if (40 < cl < 100) and (0 < si < 40) and ( 0 < sa < 45):
-        texture = 'CLAY'
+        texture = 'clay'
         WP2FC = 0.71
+
+    return np.array([texture,WP2FC])
